9c022e706a894da9b5f7232f4011799a52938e70
proto <proto@localhost.localdomain>
2020-08-28 12:00:50 +0200

    all human part end(now need to do a bot and maybe a graphical part)

--- connect4.py
+++ connect4.py
@@ -18,11 +18,48 @@ class Game:
                 output[y].append(' ')
         return output
     
-    def get_state(self):
+    def _check_right(self, pos, token, power):
+        x, y = pos
+        if x + 1 >= self.weight or self.board[y][x + 1] != token:
+            return power
+        else:
+            return self._check_right((x + 1, y), token, power + 1)
+
+    def _check_left(self, pos, token, power):
+        x, y = pos
+        if x - 1 < 0 or self.board[y][x - 1] != token:
+            return power
+        else:
+            return self._check_left((x - 1, y), token, power + 1)
+
+    def _check_top(self, pos, token, power):
+        x, y = pos
+        if y + 1 >= self.height or self.board[y + 1][x] != token:
+            return power
+        else:
+            return self._check_top((x, y + 1), token, power + 1)
+
+    def _check_bot(self, pos, token, power):
+        x, y = pos
+        if y - 1 < 0 or self.board[y - 1][x] != token:
+            return power
+        else:
+            return self._check_bot((x, y - 1), token, power + 1)
+
+    def get_state(self, pos, token):
         '''
             return if game is over or not
         '''
-        if 1 == 0:
+        if token == ' ' or pos == (-1, -1):
+            return 1
+
+        if self._check_right(pos, token, 1) >= 4:
+            return 0
+        if self._check_left(pos, token, 1) >= 4:
+            return 0
+        elif self._check_top(pos, token, 1) >= 4:
+            return 0
+        elif self._check_bot(pos, token, 1) >= 4:
             return 0
         return 1
 
@@ -30,12 +67,16 @@ class Game:
         '''
             refresh game with player turn
         '''
-        pass
+        for y, place in reversed(list(enumerate(self.board))):
+            if place[column] == ' ':
+                place[column] = token
+                return column, y
 
     def display(self):
         '''
             show the progress of the game
         '''
+        print()
         for y in range(self.height):
             print('#', end='')
             for x in range(self.weight):
@@ -47,6 +88,7 @@ class Player:
     '''
         manage player movements
     '''
+    token_lib = []
     def __init__(self):
         self.name = self._get_name()
         self.token = self._get_token()
@@ -59,31 +101,44 @@ class Player:
 
     def _get_token(self):
         token = input("Choose your token shape : ")
-        if len(token) != 1:
+        if len(token) != 1 or any(token in s for s in Player.token_lib) or token == ' ':
+            print("Token already taken or invalid (valid is one character except space")
             token = self._get_token()
+        Player.token_lib.append(token)
         return token
     
     def _validate_column(self, column):
+        if column < 1 or column > game.weight:
+            print("Out of range")
+            return 0
+        if game.board[0][column - 1] != ' ':
+            print("Already filled")
+            return 0
         return 1
 
     def turn(self):
         '''
             get input to make player's turn
         '''
-        column = input("{name} playing, which column ? (1-{end}) -> ".format(name=self.name, end=game.weight))
+        column = int(input("{name} playing, which column ? (1-{end}) -> ".format(name=self.name, end=game.weight)))
         if not self._validate_column(column):
             return self.turn()
-        return self.token, column
+        return self.token, column - 1
 
 if __name__ == '__main__':
     game = Game()
     p1 = Player()
     p2 = Player()
     players = [p1, p2]
-    o = 0
 
-    while game.get_state():
-        print()
-        game.display()
-        game.action(players[o].turn())
+    o = 1
+    pos = (-1, -1)
+    token = ' '
+
+    while game.get_state(pos, token):
         o = -o + 1
+        game.display()
+        token, column = players[o].turn()
+        pos = game.action(token, column)
+    game.display()
+    print("\n{player} win this game, GG!!".format(player=players[o].name))
